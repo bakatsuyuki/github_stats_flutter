503c10687bcbad28a3bb40f56b77687b4e9c93ee
bakatsuyuki <bakatsuyuki@icloud.com>
2022-04-10 10:32:49 +0900

    Add GQL response data as helper

---
+++ test/helper/github_graphql_response.dart
@@ -0,0 +1,59 @@
+final responseMap = {
+  "__typename": "Query",
+  "user": {
+    "__typename": "User",
+    "name": "Katsuyuki Ouchi",
+    "login": "bakatsuyuki",
+    "contributionsCollection": {
+      "__typename": "ContributionsCollection",
+      "totalCommitContributions": 2005,
+      "restrictedContributionsCount": 0
+    },
+    "repositoriesContributedTo": {
+      "__typename": "RepositoryConnection",
+      "totalCount": 5
+    },
+    "pullRequests": {"__typename": "PullRequestConnection", "totalCount": 180},
+    "openIssues": {"__typename": "IssueConnection", "totalCount": 3},
+    "closedIssues": {"__typename": "IssueConnection", "totalCount": 0},
+    "followers": {"__typename": "FollowerConnection", "totalCount": 0},
+    "repositories": {
+      "__typename": "RepositoryConnection",
+      "totalCount": 8,
+      "nodes": [
+        {
+          "__typename": "Repository",
+          "stargazers": {"__typename": "StargazerConnection", "totalCount": 0}
+        },
+        {
+          "__typename": "Repository",
+          "stargazers": {"__typename": "StargazerConnection", "totalCount": 0}
+        },
+        {
+          "__typename": "Repository",
+          "stargazers": {"__typename": "StargazerConnection", "totalCount": 0}
+        },
+        {
+          "__typename": "Repository",
+          "stargazers": {"__typename": "StargazerConnection", "totalCount": 0}
+        },
+        {
+          "__typename": "Repository",
+          "stargazers": {"__typename": "StargazerConnection", "totalCount": 0}
+        },
+        {
+          "__typename": "Repository",
+          "stargazers": {"__typename": "StargazerConnection", "totalCount": 0}
+        },
+        {
+          "__typename": "Repository",
+          "stargazers": {"__typename": "StargazerConnection", "totalCount": 0}
+        },
+        {
+          "__typename": "Repository",
+          "stargazers": {"__typename": "StargazerConnection", "totalCount": 0}
+        }
+      ]
+    }
+  }
+};
